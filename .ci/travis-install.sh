070dd445a2daa5805644c46bdf1d56f156dc674f
Sylvain Bellemare <sbellem@gmail.com>
2017-08-28 22:19:40 +0200

    Add flake8 check in travis
    closes #33

--- .ci/travis-install.sh
+++ .ci/travis-install.sh
@@ -2,16 +2,19 @@
 
 pip install --upgrade pip
 
-# will not be needed for flake8
-git clone https://github.com/JHUISI/charm.git
-cd charm && git checkout 2.7-dev
-./configure.sh
-python setup.py install
-cd ..
+if [ "${BUILD}" != "flake8" ]; then
+    git clone https://github.com/JHUISI/charm.git
+    cd charm && git checkout 2.7-dev
+    ./configure.sh
+    python setup.py install
+    cd ..
+fi
 
 if [ "${BUILD}" == "tests" ]; then
     pip install -e .[test]
     pip install --upgrade codecov
 elif [ "${BUILD}" == "docs" ]; then
     pip install -e .[docs]
+elif [ "${BUILD}" == "flake8" ]; then
+    pip install flake8
 fi
--- .ci/travis-script.sh
+++ .ci/travis-script.sh
@@ -4,4 +4,6 @@ if [ "${BUILD}" == "tests" ]; then
     pytest -v --cov=honeybadgerbft test/
 elif [ "${BUILD}" == "docs" ]; then
     sphinx-build -W -c docs -b html -d docs/_build/doctrees docs docs/_build/html
+elif [ "${BUILD}" == "flake8" ]; then
+    flake8 --count --statistics honeybadgerbft/ test/
 fi
